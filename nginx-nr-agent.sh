52c6f73cd49ec7490eeb06e3c73f9276cc92df78
Helton Damascena Dias <heltondd@gmail.com>
2019-01-14 10:33:49 -0200

    NGINX NewRelic Agent

---
+++ nginx-nr-agent.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sed -i "s|\$sed\.NEWRELIC_LICENSE|$NEWRELIC_LICENSE|g; \
+  s|\$sed\.NEWRELIC_APP|$NEWRELIC_APP|g; \
+  s|\$sed\.NGINX_STATUS_URL|$NGINX_STATUS_URL|g" /etc/nginx-nr-agent/nginx-nr-agent.ini \
+  && /usr/bin/nginx-nr-agent.py -c /etc/nginx-nr-agent/nginx-nr-agent.ini -f start
